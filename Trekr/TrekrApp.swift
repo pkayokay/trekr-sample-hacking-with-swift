0414ee6ca39806a8223cf2a528450b9457b9f46b
Paul Kim <paulk@hey.com>
2021-04-10 12:45:02 -0400

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by paulkim on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
