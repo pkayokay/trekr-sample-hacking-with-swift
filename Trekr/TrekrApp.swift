12040d96eed3c6ad01d3a1a59c0adc9443fa7671
Paul Kim <paulk@hey.com>
2021-04-14 08:28:56 -0400

    Make locations object variable to load once

--- Trekr/TrekrApp.swift
+++ Trekr/TrekrApp.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 @main
 struct TrekrApp: App {
+    @StateObject var locations = Locations()
+
     var body: some Scene {
         WindowGroup {
             TabView {
                 NavigationView {
-                    ContentView(location: Locations().primary)
+                    ContentView(location: locations.primary)
                 }
                 .tabItem {
                     Image(systemName: "airplane.circle.fill")
