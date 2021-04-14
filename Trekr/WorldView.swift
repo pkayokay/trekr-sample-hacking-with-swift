65ee70243fd6690aca54d72bdeff9d7228765501
Paul Kim <paulk@hey.com>
2021-04-14 08:36:55 -0400

    Add map flag annotation

--- Trekr/WorldView.swift
+++ Trekr/WorldView.swift
@@ -9,6 +9,7 @@ import MapKit
 import SwiftUI
 
 struct WorldView: View {
+    @EnvironmentObject var locations: Locations
     @State var region = MKCoordinateRegion(
         center:CLLocationCoordinate2D(
             latitude: 51.507222,
@@ -17,7 +18,16 @@ struct WorldView: View {
         span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40)
     )
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region, annotationItems: locations.places) {
+            location in
+            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
+                Image(location.country)
+                    .resizable()
+                    .cornerRadius(10)
+                    .frame(width: 80, height: 40)
+                    .shadow(radius: 3)
+            }
+        }
             .navigationTitle("Locations")
     }
 }
